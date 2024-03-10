2144677fca101131732326664d4ce5ae19bcf9f6
June Park <junepark202012@gmail.com>
2024-03-11 00:14:59 +0900

    refactor: extract parsing logic

--- internal/parser/parse.go
+++ internal/parser/parse.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sjunepark/gohwp/internal/hwp"
 	"github.com/sjunepark/gohwp/internal/models"
 	"os"
+	"strings"
 )
 
 func Parse(filePath string) error {
@@ -28,76 +29,118 @@ func Parse(filePath string) error {
 
 	doc := &hwp.HWPDocument{}
 
-	h, err := getHeader(reader)
+	documentData, err := getDocumentData(reader)
+
+	h, err := getHeader(documentData.header)
 	if err != nil {
 		return err
 	}
 	doc.Header = h
 
-	di, err := getDocInfo(reader)
+	di, err := getDocInfo(documentData.docInfo)
 	if err != nil {
 		return err
 	}
 	doc.DocInfo = di
 
+	s, err := getSections(documentData.bodyText)
+	if err != nil {
+		return err
+	}
+	doc.BodyText = s
+
 	return nil
 }
 
-func getHeader(reader *mscfb.Reader) (*models.HWPHeader, error) {
-	for entry, err := reader.Next(); err == nil; entry, err = reader.Next() {
-		if entry.Name == "FileHeader" {
-			headerData := make([]byte, entry.Size)
-			_, err := reader.Read(headerData)
-			if err != nil {
-				return nil, err
-			}
-
-			header, err := models.NewHWPHeader(headerData)
-			if err != nil {
-				return nil, err
-			}
-
-			supportedSignature := "HWP Document File"
-			if header.Signature != supportedSignature {
-				return nil, fmt.Errorf("unsupported signature: %s", header.Signature)
-			}
-
-			supportedVersion := models.HWPVersion{Major: 5, Minor: 0}
-			if header.Version.IsCompatible(supportedVersion) == false {
-				return nil, fmt.Errorf("unsupported version: %s", header.Version)
-			}
-			return header, nil
-		}
-	}
-	return nil, fmt.Errorf("FileHeader not found")
+type documentData struct {
+	header   []byte
+	docInfo  []byte
+	bodyText []sectionData
 }
 
-func getDocInfo(reader *mscfb.Reader) (*docInfo.DocInfo, error) {
+type sectionData []byte
+
+func getDocumentData(reader *mscfb.Reader) (*documentData, error) {
+	d := &documentData{}
+
 	for entry, err := reader.Next(); err == nil; entry, err = reader.Next() {
-		if entry.Name == "DocInfo" {
-			docInfoData := make([]byte, entry.Size)
-			_, err := reader.Read(docInfoData)
+		entryName := entry.Name
+		switch {
+		case entryName == "FileHeader":
+			data, err := getData(reader, entry.Size)
 			if err != nil {
 				return nil, err
 			}
-
-			// Raw deflate algorithm
-			deCompressedData, err := DecompressDeflate(docInfoData)
+			d.header = data
+		case entryName == "DocInfo":
+			data, err := getData(reader, entry.Size)
 			if err != nil {
 				return nil, err
 			}
-
-			docInfoParser, err := docInfo.NewParser(deCompressedData)
+			d.docInfo = data
+		// Starts with Section
+		case strings.HasPrefix(entryName, "Section"):
+			data, err := getData(reader, entry.Size)
 			if err != nil {
 				return nil, err
 			}
-
-			di, err := docInfoParser.Parse()
-			if err != nil {
-				return nil, err
-			}
-			return di, nil
+			d.bodyText = append(d.bodyText, data)
 		}
 	}
-	return nil, fmt.Errorf("DocInfo not found")
+	return d, nil
+}
+
+func getData(reader *mscfb.Reader, size int64) ([]byte, error) {
+	data := make([]byte, size)
+	_, err := reader.Read(data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func getHeader(data []byte) (*models.HWPHeader, error) {
+	header, err := models.NewHWPHeader(data)
+	if err != nil {
+		return nil, err
+	}
+
+	supportedSignature := "HWP Document File"
+	if header.Signature != supportedSignature {
+		return nil, fmt.Errorf("unsupported signature: %s", header.Signature)
+	}
+
+	supportedVersion := models.HWPVersion{Major: 5, Minor: 0}
+	if header.Version.IsCompatible(supportedVersion) == false {
+		return nil, fmt.Errorf("unsupported version: %s", header.Version)
+	}
+	return header, nil
+}
+
+func getDocInfo(data []byte) (*docInfo.DocInfo, error) {
+	deCompressedData, err := DecompressDeflate(data)
+	if err != nil {
+		return nil, err
+	}
+
+	docInfoParser, err := docInfo.NewParser(deCompressedData)
+	if err != nil {
+		return nil, err
+	}
+
+	di, err := docInfoParser.Parse()
+	if err != nil {
+		return nil, err
+	}
+	return di, nil
+}
+
+func getSections(data []sectionData) ([]*models.Section, error) {
+	sections := make([]*models.Section, len(data))
+	for _, sectionData := range data {
+		// todo: parse sectionData
+		// todo: append section to sections
+		fmt.Println(sectionData)
+	}
+	return sections, nil
 }
