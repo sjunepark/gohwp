21b0f63f69ce43f0f41862cf9d2881b185a94520
June Park <junepark202012@gmail.com>
2024-03-13 07:20:50 +0900

    refactor: extract DivisionType

--- internal/reader/model/paraHeader.go
+++ internal/reader/model/paraHeader.go
@@ -4,30 +4,6 @@ import (
 	"fmt"
 )
 
-type DivisionType uint8
-
-const (
-	DivisionTypeSection DivisionType = iota
-	DivisionTypeMultiColumn
-	DivisionTypePage
-	DivisionTypeColumn
-)
-
-func (dt DivisionType) String() string {
-	switch dt {
-	case DivisionTypeSection:
-		return "Section"
-	case DivisionTypeMultiColumn:
-		return "MultiColumn"
-	case DivisionTypePage:
-		return "Page"
-	case DivisionTypeColumn:
-		return "Column"
-	default:
-		return fmt.Sprintf("Unknown DivisionType (%d)", dt)
-	}
-}
-
 type ParaHeaderV1 struct {
 	TextLength       uint32
 	ControlMask      ControlMask
